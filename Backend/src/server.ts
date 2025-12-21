39758fc209486cced28f3f4a496870dfc8317795
Kushal Poudel <wrckushal@gmail.com>
2025-12-21 15:52:33 +0545

    Update server.ts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://task-toaster.vercel.app",
   credentials: true
 }));
 app.use(express.json());
